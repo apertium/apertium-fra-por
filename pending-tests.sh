1f14c8fd56cd53acd6f4e94b301ecb0b4347cdce
Francis M. Tyers <ftyers@prompsit.com>
2010-08-17 21:18:28 +0000

    remove some proper nouns to the pending section

--- pending-tests.sh
+++ pending-tests.sh
@@ -4,14 +4,11 @@ C=2
 GREP='.'
 if [ $# -eq 1 ]
 then
-C=$1
-GREP='WORKS'
+    C=$1
+    GREP='WORKS'
 fi
 
-./wiki-tests.sh Pending fr pt  | grep -C $C "$GREP"
-
-echo ""
-
-./wiki-tests.sh Pending pt fr  | grep -C $C "$GREP"
+sh wiki-tests.sh Pending fr pt update | grep -C $C "$GREP"
+sh wiki-tests.sh Pending pt fr update | grep -C $C "$GREP"
 
 
